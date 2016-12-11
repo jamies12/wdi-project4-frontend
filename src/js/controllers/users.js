f2cd95eecd9ab45e0ae34adea7507b6b85d6e0b1
Jamie Simpson <jamie.shaun.simpson12@gmail.com>
2016-12-11 13:35:12 +0000

    Initial commit/basic front end set up

---
+++ src/js/controllers/users.js
@@ -0,0 +1,9 @@
+angular.module('spaces')
+  .controller('UsersIndexController', UsersIndexController);
+
+UsersIndexController.$inject = ['User'];
+function UsersIndexController(User) {
+  const usersIndex = this;
+
+  usersIndex.all = User.query();
+}
